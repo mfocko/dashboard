8bb7ef8d667c10c322fff8321f98a4f8991f44df
Matej Focko <mfocko@redhat.com>
2023-07-10 10:10:21 +0200

    don't post comments
    Signed-off-by: Matej Focko <mfocko@redhat.com>
    (cherry picked from commit 42ef4dd9d74fd848fcff216a002f0ac52c6fe6e5)

--- .github/notify.py
+++ .github/notify.py
@@ -9,30 +9,7 @@ from ogr import GithubService
 
 
 def main():
-    if len(sys.argv) != 2:
-        logging.error("Usage: notify domain")
-        sys.exit(1)
-
-    time = datetime.datetime.utcnow().strftime("%a %d %b %Y, %H:%M UTC")
-    domain = sys.argv[1]
-
-    service = GithubService(token=os.getenv("GH_PR_TOKEN"))
-    owner, repo = (os.getenv("GITHUB_REPOSITORY") or "").split("/")
-    pr_id = int(os.getenv("GH_PR_NUM"))
-
-    project = service.get_project(namespace=owner, repo=repo)
-    pr = project.get_pr(pr_id)
-    comments = pr.get_comments(author=service.user.get_username())
-
-    body = f"Preview: https://{domain} (deployed at {time})"
-
-    if comments:
-        comments[0].body = body
-        logging.info("Updated existing comment")
-        return
-
-    pr.comment(body=body)
-    logging.info("Created new comment")
+    logging.info("screw you")
 
 
 if __name__ == "__main__":
